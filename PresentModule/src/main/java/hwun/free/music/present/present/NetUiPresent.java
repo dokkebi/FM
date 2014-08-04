6aa29a1e42758badcf98cf41a66ce5fb6da61771
HWUN <honghwun@gmail.com>
2014-08-05 01:12:41 +0900

    Test

--- PresentModule/src/main/java/hwun/free/music/present/present/NetUiPresent.java
+++ PresentModule/src/main/java/hwun/free/music/present/present/NetUiPresent.java
@@ -1,13 +1,17 @@
 package hwun.free.music.present.present;
 import hwun.free.music.model.HttpUtil;
+import hwun.free.music.present.ifs.INetUi;
+
 /**
  * Created by HWUN on 2014-08-04.
  */
-public class NetUiPresent {
+public class NetUiPresent implements INetUi {
     private HttpUtil httpUtil;
-    public String getHtmlSource(){
+
+    @Override
+    public void getHtmlSource(String targetUrl) {
         httpUtil = new HttpUtil();
         httpUtil.getHtmlSource("");
-        return "";
     }
+
 }
