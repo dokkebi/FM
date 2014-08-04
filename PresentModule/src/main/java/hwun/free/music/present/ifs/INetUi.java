187541eac96618fa54eb472caf9b02293964b693
HWUN <honghwun@gmail.com>
2014-08-05 01:08:52 +0900

    Test

--- PresentModule/src/main/java/hwun/free/music/present/ifs/INetUi.java
+++ PresentModule/src/main/java/hwun/free/music/present/ifs/INetUi.java
@@ -4,5 +4,8 @@ package hwun.free.music.present.ifs;
  * Created by HWUN on 2014-08-04.
  */
 public interface INetUi {
+    /**
+     * @param targetUrl
+     */
     void getHtmlSource(String targetUrl);
 }
