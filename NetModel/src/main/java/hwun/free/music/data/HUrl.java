7447483b9bbec9afff05fa8fb48f9e08ec1a165a
HWUN <honghwun@gmail.com>
2014-08-05 21:55:02 +0900

    Test

---
+++ NetModel/src/main/java/hwun/free/music/data/HUrl.java
@@ -0,0 +1,8 @@
+package hwun.free.music.data;
+
+/**
+ * Created by HWUN on 2014-08-05.
+ */
+public class HUrl {
+    public static final String SEARCH_BASE_URL = "http://search.4shared.com/q/CCQD/1/music/Touch+my";
+}
